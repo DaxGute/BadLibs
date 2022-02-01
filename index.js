82a8492188ceb9bcf98a3a9f1b0eba64e84f9405
DaxGute <daxton@gutekunst.com>
2022-01-31 17:34:35 -0800

    Back to ground 0

--- index.js
+++ index.js
@@ -1,58 +1,3 @@
-// 'use strict';
-
-// const express = require('express');
-// const app = require('express')();
-// app.set('view engine', 'pug');
-
-// const server = require('http').Server(app);
-// const io = require('socket.io')(server);
-
-// app.get('/', (req, res) => {
-//   res.render("home.ejs")
-// });
-
-
-// const {PythonShell} = require('python-shell');
-
-// io.on('connection', socket => {
-//     let options = {
-//       mode: 'text',
-//       pythonOptions: ['-u'], // get print results in real-time
-//       args: [''] //An argument which can be accessed in the script using sys.argv[1]
-//     };
-//     var dataVar
-    
-//     PythonShell.run('./src/pysrc/getNewTitle.py', options, function (err, result){
-//       if (err) throw err;
-//       dataVar = result.toString()
-//       socket.emit('getArticle', dataVar)
-//     });
-//     socket.on('anotherArticle', () => {
-//       PythonShell.run('./src/pysrc/getNewTitle.py', options, function (err, result){
-//         if (err) throw err;
-//         dataVar = result.toString()
-//         socket.emit('getArticle', dataVar)
-//       });
-//     })
-// })
-
-// if (module === require.main) {
-//   const PORT = process.env.PORT || 8080;
-//   server.listen(PORT, () => {
-//     console.log(`App listening on port ${PORT}`);
-//     console.log('Press Ctrl+C to quit.');
-//   });
-// }
-
-// // server.listen(8080, () => {
-// //   console.log("Server Running")
-// // })
-// // // [END appengine_websockets_app]
-
-// module.exports = server;
-
-
-
 'use strict';
 
 // [START appengine_websockets_app]
@@ -72,28 +17,26 @@ app.get('/', (req, res) => {
 
 const {PythonShell} = require('python-shell');
 io.on('connection', socket => {
-  socket.on('chat message', msg => {
-    let options = {
-      mode: 'text',
-      pythonOptions: ['-u'], // get print results in real-time
-      args: [''] //An argument which can be accessed in the script using sys.argv[1]
-    };
-    var dataVar
-    
+  let options = {
+    mode: 'text',
+    pythonOptions: ['-u'], // get print results in real-time
+    args: [''] //An argument which can be accessed in the script using sys.argv[1]
+  };
+  var dataVar
+  
 
-    PythonShell.run('./src/pysrc/testing.py', options, function (err, result){
+  PythonShell.run('./src/pysrc/getNewTitle.py', options, function (err, result){
+    if (err) throw err;
+    var dataVar = result.toString()
+    socket.emit('getArticle', dataVar)
+  });
+  socket.on('anotherArticle', () => {
+    PythonShell.run('./src/pysrc/getNewTitle.py', options, function (err, result){
       if (err) throw err;
-      var dataVar = result.toString()
-      socket.emit('chat message', dataVar)
+      dataVar = result.toString()
+      socket.emit('getArticle', dataVar)
     });
-    // socket.on('anotherArticle', () => {
-    //   PythonShell.run('./src/pysrc/getNewTitle.py', options, function (err, result){
-    //     if (err) throw err;
-    //     dataVar = result.toString()
-    //     socket.emit('getArticle', dataVar)
-    //   });
-    // })
-  })  
+  })
 });
 
 if (module === require.main) {
